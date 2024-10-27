53f6c4ebbcaf9e549a194f5bb49edfadef5fb40c
eu <eu.nyao.9@gmail.com>
2024-10-28 00:38:12 +0900

    一覧用データ、フォント

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ['class'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {}
-    	}
-    },
+		extend: {
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			colors: {},
+			fontFamily: {
+				sans: ['Noto Sans JP Variable', "sans-serif", ...defaultTheme.fontFamily.sans],
+				inter: ['Inter Variable', "sans-serif", ...defaultTheme.fontFamily.sans],
+			},
+		}
+	},
 	plugins: [require("tailwindcss-animate")],
 }
